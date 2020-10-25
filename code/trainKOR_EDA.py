a26b685a095b53ecd430327002cc95bc6efdfcbb
liberatedwinner <kale.lbnr@gmail.com>
2020-10-25 14:24:23 +0900

    n

--- code/trainKOR_EDA.py
+++ code/trainKOR_EDA.py
@@ -1,19 +1,31 @@
 import pandas as pd
 
-# csv read
-# 시간축에 따른 크기 그래프를 그려본다
-# 어느 것이 속도랑 연관이 있을까 한번 재어봐야지
-# 내 생각엔 P/B인데
-
 #df = pd.read_excel('..//Data//trainKOR//180713_2.xlsx')
 df = pd.read_csv('..//Data//trainKOR//180713_2.csv')
-print(df.columns) # 한글로 된 거 영어로 바꿔야됨
-df.info()
-print(df.shape)
-print(df.dtypes)
-df.describe() # 통계값들
-df.count() # 몇 개쯤 있나
-df.corr() # 상관계수
 
+df.drop(['번호',
+         'OP Mode',
+         '편성번호',
+         '열차길이',
+         'VOBC ＃1',
+         'VOBC ＃0',
+         'Unnamed: 27',
+         'Unnamed: 28'
+        ], axis=1, inplace=True)
 
+df.rename(columns={'시간': 'time'}, inplace=True)
+df.columns = df.columns.str.lower()
 
+df['p/b'] = df['p/b'].str[:-3]
+df['p/b'] = df['p/b'].astype('int64')
+
+df['time'] = df['time'].str.replace(':', '')
+df['time'] = df['time'].astype('int64')
+
+df['distance'] = df['distance'].str[:-3]
+df['distance'] = df['distance'].astype('int64')
+
+speedwords = ['target', 'permitted', 'actual', 'train']
+for word in speedwords:
+    df[f'{word} speed'] = df[f'{word} speed'].str[:-6]
+    df[f'{word} speed'] = df[f'{word} speed'].astype('int64')
