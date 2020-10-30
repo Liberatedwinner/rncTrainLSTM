063e4682b37eb1beb30fa061082217897c9d545a
liberatedwinner <kale.lbnr@gmail.com>
2020-10-30 18:30:14 +0900

    ;

--- code/metroKOR_feature_engineering.py
+++ code/metroKOR_feature_engineering.py
@@ -1,13 +1,9 @@
 import numpy as np
 import pandas as pd
 import argparse
+import pickle
 
 np.random.seed(20201005)
-
-# df = pd.read_excel('..//Data//trainKOR//180713_2.xlsx')
-filenames = ['20180713.csv', '20180717.csv']
-filename = '20180713.csv'
-df = pd.read_csv(f'..//Data//metroKOR//{filename}')
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--predictstep', type=int, default=10,
@@ -15,34 +11,103 @@ parser.add_argument('--predictstep', type=int, default=10,
 args = parser.parse_args()
 predicted_step = args.predictstep
 
-df.rename({'BC ＃1': 'BC1', 'BC ＃2': 'BC2', 'BC ＃3': 'BC3',
-           'BC ＃4': 'BC4', 'BC ＃7': 'BC5', 'BC ＃0': 'BC6'},
-          axis=1, inplace=True)
+def save_data(data, filename=None):
+    assert filename, "Invalid file name."
+    print("=======")
+    print(f'Saving the data to a filename {filename}...')
+    with open(filename, 'wb') as f:
+        pickle.dump(data, f)
+    print("The data has been saved.")
+    print("=======")
 
-df.drop(['번호', 'OP Mode', '편성번호', '열차길이', 'VOBC ＃1', 'VOBC ＃0',
-         'Master Clock of VOBC', 'Train In Station.1',
-         'Next Platform ID', 'Final Platform ID',
-         'Unnamed: 27', 'Unnamed: 28', 'Target Speed',
-         'Train Room Temp ＃1', 'Train Outside Temp ＃1',
-         'FWD', 'REV', 'Train In Station', 'Line Voltage',
-         'DISTANCE', 'MR Pressure', 'Distance to Target'],
-        axis=1, inplace=True)
 
-df.rename(columns={'시간': 'time'}, inplace=True)
-df.columns = df.columns.str.lower()
-df['time'] = pd.to_datetime(filename[:-4] + df['time'].str.replace(':', ''))
+def load_data(filename=None):
+    assert filename, "Invalid file name."
+    print("=======")
+    print('Now loading...')
+    with open(filename, 'rb') as f:
+        data = pickle.load(f)
+    print("Complete.")
+    print("=======")
+    return data
 
-df['p/b'] = df['p/b'].str[:-3]
-df['p/b'] = df['p/b'].astype('int64')
 
-speedwords = ['permitted', 'actual', 'train']
-for word in speedwords:
-    df[f'{word} speed'] = df[f'{word} speed'].str[:-6]
-    df[f'{word} speed'] = df[f'{word} speed'].astype('int64')
+def preprocessing(nrows=None):
+    filenames = ['20180713.csv', '20180717.csv']
+    filename = '20180713.csv'
+    df = pd.read_csv(f'..//Data//metroKOR//{filename}')
 
-for i in range(1, 7):
-    df[f'bc{i}'] = df[f'bc{i}'].str[:-5]
-    df[f'bc{i}'] = df[f'bc{i}'].astype('float64')
+    df.rename({'BC ＃1': 'BC1', 'BC ＃2': 'BC2', 'BC ＃3': 'BC3',
+               'BC ＃4': 'BC4', 'BC ＃7': 'BC5', 'BC ＃0': 'BC6'},
+              axis=1, inplace=True)
+
+    df.drop(['번호', 'OP Mode', '편성번호', '열차길이', 'VOBC ＃1', 'VOBC ＃0',
+             'Master Clock of VOBC', 'Train In Station.1',
+             'Next Platform ID', 'Final Platform ID',
+             'Unnamed: 27', 'Unnamed: 28', 'Target Speed',
+             'Train Room Temp ＃1', 'Train Outside Temp ＃1',
+             'FWD', 'REV', 'Train In Station', 'Line Voltage',
+             'DISTANCE', 'MR Pressure', 'Distance to Target'],
+            axis=1, inplace=True)
+
+    df.rename(columns={'시간': 'time'}, inplace=True)
+    df.columns = df.columns.str.lower()
+    df['time'] = pd.to_datetime(filename[:-4] + df['time'].str.replace(':', ''))
+
+    df['p/b'] = df['p/b'].str[:-3]
+    df['p/b'] = df['p/b'].astype('int64')
+
+    speedwords = ['permitted', 'actual', 'train']
+    for word in speedwords:
+        df[f'{word} speed'] = df[f'{word} speed'].str[:-6]
+        df[f'{word} speed'] = df[f'{word} speed'].astype('int64')
+
+    for i in range(1, 7):
+        df[f'bc{i}'] = df[f'bc{i}'].str[:-5]
+        df[f'bc{i}'] = df[f'bc{i}'].astype('float64')
+
+    return df
+
+### TODO
+def feature_engineering(dataAll, predictStep=[10]):
+    FLAG = dataAll["FLAG"].unique()
+    newData = []
+
+    print("=======")
+    for flag in FLAG:
+        print("Start with the file {}:".format(flag))
+        data = dataAll[dataAll["FLAG"] == flag]
+
+        data.reset_index(inplace=True, drop=True)
+        data.reset_index(inplace=True)
+        data.rename({"index": "timeStep"}, axis=1, inplace=True)
+
+        print("lagging features")
+        df = lagging_features(df, name="actual speed", laggingStep=list(range(1, 11)) + [20, 30, 50, 80])
+        df = lagging_features(df, name="p/b", laggingStep=list(range(1, 6)) + [20, 60])
+
+        df['train speed_mult_0'] = df['actual speed']
+        for k in range(1, 6):
+            df[f'train speed_mult_{k}'] = df[f'train speed_mult_{k - 1}'] * df[f'lagged_speed_{k}']
+
+        print("statistical features")
+        for k in [5, 10, 20]:
+            df = statistical_features(df, name='actual speed', timeRange=k)
+            df = statistical_features(df, name='p/b', timeRange=k)
+
+        print("the time step flag with the target")
+        df = create_target(df, predictStep=predicted_step, targetName="actual speed")
+
+        data = data[~data["target"].isnull()]
+        data.reset_index(inplace=True, drop=True)
+        newData.append(data)
+
+    print("=======")
+    return newData
+
+
+
+### TODO
 
 
 def lagging_features(data, name=None, laggingStep=[1, 2, 3]):
@@ -56,7 +121,7 @@ def lagging_features(data, name=None, laggingStep=[1, 2, 3]):
     return data
 
 
-def statistical_features(data, name=None, operation=["mean", "std"], timeRange=5):
+def statistical_features(data, name=None, timeRange=5):
     assert name, "Invalid feature name!"
     index = list(data.index)
     featureValues = data[name].values
@@ -91,21 +156,40 @@ def create_target(data, predictStep=None, targetName="actual speed"):
     return newData
 
 
-df.reset_index(inplace=True, drop=True)
-df.reset_index(inplace=True)
-df.rename({"index": "timeStep"}, axis=1, inplace=True)
 
-df = lagging_features(df, name="actual speed", laggingStep=list(range(1, 11)) + [20, 30, 50, 80])
-df = lagging_features(df, name="p/b", laggingStep=list(range(1, 6)) + [20, 60])
+if __name__ == "__main__":
+    df = preprocessing()
+    dataAll = feature_engineering(dataAll, predictStep=[PREDICTED_STEP])
+    print("\nMerging the data:")
+    print("=======")
+    shapeList = [len(df) for df in dataAll]
+    print(f"Total shape is {sum(shapeList)}")
+    newData = pd.DataFrame(None, columns=list(dataAll[0].columns))
+    for data in dataAll:
+        print(f"Now the shape is {len(data)}.")
+        newData = pd.concat([newData, data], axis=0, ignore_index=True)
+    print("=======")
 
-df['train speed_mult_0'] = df['actual speed']
-for k in range(1, 6):
-    df[f'train speed_mult_{k}'] = df[f'train speed_mult_{k-1}'] * df[f'lagged_speed_{k}']
+
+    # Saved all the data
+    PATH = f"..//Data//TrainedRes//sec{PREDICTED_STEP}//"
+
+    if not os.path.exists(PATH):
+        os.makedirs(PATH)
+    ls = LoadSave()
+    ls.save_data(data=newData[(newData["FLAG"] == 0)],
+                 path=PATH + "Train.pkl")
+
+    ls.save_data(path=PATH + "Test.pkl",
+                 data=newData[(newData["FLAG"] == 1)].drop("target", axis=1))
+
+    ls.save_data(path=PATH + "TestResults.pkl",
+                 data=newData[(newData["FLAG"] == 1)]["target"].values)
 
 
-for k in [5, 10, 20]:
-    df = statistical_features(df, name='actual speed', timeRange=k)
-    df = statistical_features(df, name='p/b', timeRange=k)
 
-df = create_target(df, predictStep=predicted_step, targetName="actual speed")
+
+
+
+
 
