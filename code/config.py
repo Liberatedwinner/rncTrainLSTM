088fe3c7293d0c32e61c10c41b8df35dd51a400f
liberatedwinner <kale.lbnr@gmail.com>
2020-11-16 15:30:47 +0900

    save

---
+++ code/config.py
@@ -0,0 +1,7 @@
+MODEL_CONFIG = {
+    'path': f'..//Data//TrainedRes//',
+    'fold_number': 10,
+    'hidden_sizes': [10, 14, 18, 22, 26, 30],
+    'learning_rates': [1e-5, 1e-4, 2e-4, 5e-4],
+    'batch_sizes': [32, 64, 128, 256, 512]
+}
