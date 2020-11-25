8269a96601b5ef44598cebe0f68056461f54d278
liberatedwinner <kale.lbnr@gmail.com>
2020-11-25 23:12:54 +0900

    night

--- code/metroLSTM_dynamic_model.py
+++ code/metroLSTM_dynamic_model.py
@@ -305,14 +305,12 @@ def evaluate_model(_train_data, _test_data,
     y_valid_pred = model.predict(X_valid)
     y_test = y_sc.inverse_transform(y_test)
     y_test_pred = model.predict(X_test)
-
     if basemodel_switch:
         y_valid_pred = y_sc.inverse_transform(y_valid_pred.reshape(-1, 1))
         y_test_pred = y_sc.inverse_transform(y_test_pred.reshape(-1, 1))
     else:
         y_valid_pred = y_sc.inverse_transform(y_valid_pred)
         y_test_pred = y_sc.inverse_transform(y_test_pred)
-
     y_valid_pred[y_valid_pred < 1] = 0
     y_test_pred[y_test_pred < 1] = 0
 
